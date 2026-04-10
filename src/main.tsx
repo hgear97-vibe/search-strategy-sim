f30c5f9f043fad9625dc9861bb3b74abf8f79d0f
Harshith <hg2702@columbia.edu>
2026-04-10 00:47:16 -0400

    Trigger Vercel redeploy from latest commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,5 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+// Deployment trigger: Fri Apr 10 00:47:10 EDT 2026
+// Deployment trigger: Fri Apr 10 00:47:16 EDT 2026
